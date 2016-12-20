bb89ebffaa86c87a6872e87df9aceba4bf018e4d
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-12-20 20:01:47 +0545

    stateful example

--- App.jsx
+++ App.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 
 class App extends React.Component {
-    render() {
-        var i = 1;
+    constructor() {
+        super();
 
-        var myStyle = {
-            fontSize: 100,
-            color: '#FF0000'
-        };
+        this.state = {
+            data: [
+                {
+                    "id":1,
+                    "name":"Foo",
+                    "age":"20"
+                },
+                {
+                    "id":2,
+                    "name":"Bar",
+                    "age":"30"
+                },
+                {
+                    "id":3,
+                    "name":"Baz",
+                    "age":"40"
+                },
+            ]
+        }
+    }
+
+    render() {
         return (
            <div>
-               Hello World!
-               <h1>{1 + 1}</h1>
-               <h2 style={myStyle}>{i == 1 ? 'True' : 'False'}</h2>
-
                <Header/>
-               <Content/>
+               <table>
+                   <tbody>
+                        {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+                   </tbody>
+               </table>
            </div>
         );
     }
@@ -25,19 +43,26 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Header!</h1>
+                <h1>Header</h1>
             </div>
         );
     }
 }
 
-class Content extends React.Component {
+class TableRow extends React.Component {
     render() {
         return (
-            <div>
-                <h2>Content</h2>
-                <p>The content text....</p>
-            </div>
+            <tr>
+                <td>
+                    {this.props.data.id}
+                </td>
+                <td>
+                    {this.props.data.name}
+                </td>
+                <td>
+                    {this.props.data.age}
+                </td>
+            </tr>
         );
     }
 }
