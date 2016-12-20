e8b815e83ba37c58aa0b8cf8b0bf657170f557fa
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-12-20 20:19:47 +0545

    default props

--- App.jsx
+++ App.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            "header": "Header from state....",
-            "content": "Content from state...."
-        }
-    }
-
     render() {
         return (
            <div>
-                <h1>{this.state.header}</h1>
-                <h2>{this.state.content}</h2>
+                <h1>{this.props.headerProp}</h1>
+                <h2>{this.props.contentProp}</h2>
            </div>
         );
     }
+}
+
+App.defaultProps = {
+    headerProp: "Header from props....",
+    contentProp: "Content from props...."
 }
 
 export default App;
