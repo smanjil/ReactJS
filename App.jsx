5d3883a4b9fdbab941762f1ba671df53a512b2b0
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-12-23 19:39:01 +0545

    Component API - findDOMNode()

--- App.jsx
+++ App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     constructor() {
         super();
 
-        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+        this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
     };
 
-    forceUpdateHandler() {
-        this.forceUpdate();
-    };
+    findDomNodeHandler() {
+        var myDiv = document.getElementById("myDiv");
+        ReactDOM.findDOMNode(myDiv).style.color = 'green';
+    }
 
     render() {
         return (
            <div>
-                <button onClick={this.forceUpdateHandler}>FORCE UPDATE</button>
-                <h4>Random Number: {Math.random()}</h4>
+                <button onClick={this.findDomNodeHandler}>FIND DOM NODE</button>
+                <div id="myDiv">NODE</div>
            </div>
         );
     }
