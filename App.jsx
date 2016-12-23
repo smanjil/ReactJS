73f911f51b7bbc81d2f81a00f2ba12fe904485b8
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-12-23 19:58:52 +0545

    component life cycle

--- App.jsx
+++ App.jsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor() {
-        super();
 
-        this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: 0
+        }
+
+        this.setNewNumber = this.setNewNumber.bind(this)
     };
 
-    findDomNodeHandler() {
-        var myDiv = document.getElementById("myDiv");
-        ReactDOM.findDOMNode(myDiv).style.color = 'green';
+    setNewNumber() {
+        this.setState({data: this.state.data + 1})
     }
 
     render() {
         return (
-           <div>
-                <button onClick={this.findDomNodeHandler}>FIND DOM NODE</button>
-                <div id="myDiv">NODE</div>
-           </div>
+            <div>
+                <button onClick = {this.setNewNumber}>INCREMENT</button>
+                <Content myNumber = {this.state.data}></Content>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+
+    componentWillMount() {
+        console.log('Component WILL MOUNT!')
+    }
+
+    componentDidMount() {
+        console.log('Component DID MOUNT!')
+    }
+
+    componentWillReceiveProps(newProps) {
+        console.log('Component WILL RECIEVE PROPS!')
+    }
+
+    shouldComponentUpdate(newProps, newState) {
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('Component WILL UPDATE!');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Component DID UPDATE!')
+    }
+
+    componentWillUnmount() {
+        console.log('Component WILL UNMOUNT!')
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>{this.props.myNumber}</h3>
+            </div>
         );
     }
 }
